23a4623b6c372a027b046d7effa3867d58ce12d6
ag7-alexis <guay.alexis7@gmail.com>
2023-03-13 14:45:51 +0100

    start back

---
+++ libs/shared-nest/src/lib/auth/index.ts
@@ -0,0 +1,7 @@
+export {
+  JwtAuthGuard,
+  JwtStrategy,
+  AllowUnauthorizedRequest,
+} from './jwt.strategy';
+export { LocalStrategy, LocalAuthGuard } from './local.strategy';
+export { AuthService } from './auth.service';
