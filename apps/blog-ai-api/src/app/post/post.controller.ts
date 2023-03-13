403842ea0b3b459f5bac03fccaab9f08145a9cda
ag7-alexis <guay.alexis7@gmail.com>
2023-03-13 19:56:56 +0100

    crud post

--- apps/blog-ai-api/src/app/post/post.controller.ts
+++ apps/blog-ai-api/src/app/post/post.controller.ts
@@ -1,8 +1,17 @@
+import { Post } from '@challenge-vue-api-blog-ai/shared';
 import {
   AllowUnauthorizedRequest,
   JwtAuthGuard,
 } from '@challenge-vue-api-blog-ai/shared-nest';
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post as HttpPost,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 
 @UseGuards(JwtAuthGuard)
@@ -20,5 +29,15 @@ export class PostController {
   @AllowUnauthorizedRequest()
   find(@Param('uuid') uuid: string) {
     return this.service.retrieveOne(uuid);
+  }
+
+  @HttpPost()
+  create(@Body() postCandidate: Partial<Post>) {
+    return this.service.create(postCandidate);
+  }
+
+  @Put(':uuid')
+  edit(@Param('uuid') uuid: string, @Body() postCandidate) {
+    return this.service.update(uuid, postCandidate);
   }
 }
