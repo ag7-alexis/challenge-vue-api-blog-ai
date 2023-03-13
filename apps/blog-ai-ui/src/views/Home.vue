ccc324efbefe8ece59b25947b97d21778b1c3d05
mathieusi38 <mathieusi38@gmail.com>
2023-03-13 16:42:15 +0100

    Ajout last articles home page

--- apps/blog-ai-ui/src/views/Home.vue
+++ apps/blog-ai-ui/src/views/Home.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="home">
     <BannerFront msg="Bienvenue sur notre site de génération d'article" />
+    <LastArticles />
+    <v-container>
+    <v-row>
+      <v-col
+        v-for="n in 4"
+        :key="n"
+        cols="12"
+        sm="6"
+      >
+
+      <CardArticle />
+    </v-col>
+    </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import BannerFront from '../components/BannerFront.vue';
+import LastArticles from '../components/LastArticles.vue';
+import CardArticle from '../components/CardArticle.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     BannerFront,
+    LastArticles,
+    CardArticle,
   },
 });
 </script>
