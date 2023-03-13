dc94108874021105a3cf7d420b2faf4248bb9b17
mathieusi38 <mathieusi38@gmail.com>
2023-03-13 16:27:23 +0100

    Ajout section banner landing page

--- apps/blog-ai-ui/src/views/Home.vue
+++ apps/blog-ai-ui/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <BannerFront msg="Bienvenue sur notre site de génération d'article" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import BannerFront from '../components/BannerFront.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    BannerFront,
   },
 });
 </script>
--- apps/blog-ai-ui/src/views/Login.vue
+++ apps/blog-ai-ui/src/views/Login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="login">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <BannerFront msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import BannerFront from '../components/BannerFront.vue';
 
 export default defineComponent({
   name: 'Login',
   components: {
-    HelloWorld,
+    BannerFront,
   },
 });
 </script>
