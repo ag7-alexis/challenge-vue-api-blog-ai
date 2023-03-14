384e2aabbf7b378c797e5e88cebdaa3dcdd2e47a
Leo Terras <l.terras@sciencesu-lyon.fr>
2023-03-14 16:38:37 +0100

    pagination

--- apps/blog-ai-ui/src/views/Articles.vue
+++ apps/blog-ai-ui/src/views/Articles.vue
@@ -5,13 +5,13 @@
       <v-container>
         <v-row>
           <v-col
-            v-for="item in articles"
+            v-for="item in articles.data"
             :key="item.uuid"
             cols="12"
             sm="6"
           >
 
-          <CardArticle :title="item.title" :content="item.content" :categoryName="item.category.name" :uuid="item.uuid" :thumbnail="item.thumnail" />
+          <CardArticle :title="item.title" :content="item.content" :categoryName="item.category.name" :uuid="item.uuid" :thumbnail="item.thumbnail" />
 
         </v-col>
       </v-row>
@@ -24,6 +24,11 @@ import { defineComponent,ref } from 'vue';
 import CardArticle from '../components/CardArticle.vue';
 import AllArticles from '../components/AllArticles.vue';
 import axios from 'axios';
+import { Pagination, Post } from '@challenge-vue-api-blog-ai/shared';
+interface ViewContext {
+  articles:Pagination<Post>
+}
+
 
 export default defineComponent({
   name: 'Articles',
@@ -31,9 +36,9 @@ export default defineComponent({
     CardArticle,
     AllArticles
   },
-  data() {
+  data(): ViewContext {
     return {
-      articles: [],
+      articles: Pagination.empty(),
     };
   },
   methods: {
@@ -41,7 +46,7 @@ export default defineComponent({
       const API_ALL_POST = 'http://localhost:3333/api/post'
       const isDataLoading = ref(true)
 
-      const AllArticles = await axios.get(API_ALL_POST)
+      const AllArticles = await axios.get<Pagination<Post>>(API_ALL_POST)
       const {data,status} = AllArticles // object destructuring FTW!
       if(status===200){
           isDataLoading.value=false
