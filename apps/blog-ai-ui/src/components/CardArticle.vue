b00ae99c5d311a4be48078f01c8e43e286a35dcb
mathieusi38 <mathieusi38@gmail.com>
2023-03-14 09:14:21 +0100

    Ajout date card Article

--- apps/blog-ai-ui/src/components/CardArticle.vue
+++ apps/blog-ai-ui/src/components/CardArticle.vue
@@ -1,52 +1,34 @@
 <template>
     <v-card
-      class="mx-auto"
+      class="mx-auto rounded-lg"
       max-width="344"
     >
       <v-card-text>
-        <div>Word of the Day</div>
-        <p class="text-h4 text--primary">
+        <p class="text-h5 text--primary d-inline">
           el·ee·mos·y·nar·y
         </p>
-        <p>adjective</p>
-        <div class="text--primary">
+        <v-badge
+          class="d-inline"
+          color="error"
+          content="Catégorie"
+          inline
+        ></v-badge>
+        <div class="text--primary text-left mt-8">
           relating to or dependent on charity; charitable.<br>
           "an eleemosynary educational institution."
         </div>
       </v-card-text>
-      <v-card-actions>
+      <v-card-actions class="d-inline">
+        <span>13/03</span>
+      </v-card-actions>  
+      <v-card-actions class="d-inline">
         <v-btn
           variant="text"
           color="teal-accent-4"
-          @click="reveal = true"
         >
-          Learn More
+          Lire la suite
         </v-btn>
       </v-card-actions>
-  
-      <v-expand-transition>
-        <v-card
-          v-if="reveal"
-          class="v-card--reveal"
-          style="height: 100%;"
-        >
-          <v-card-text class="pb-0">
-            <p class="text-h4 text--primary">
-              Origin
-            </p>
-            <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
-          </v-card-text>
-          <v-card-actions class="pt-0">
-            <v-btn
-              variant="text"
-              color="teal-accent-4"
-              @click="reveal = false"
-            >
-              Close
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-expand-transition>
     </v-card>
   </template>
 
@@ -54,7 +36,7 @@
   import { defineComponent } from 'vue';
   
   export default defineComponent({
-    name: 'LastArticles',
+    name: 'CardArticle',
     props: {
       msg: String,
     },
